9c4d695e4c942cee900b06de2da678d2c669d5b0
kmillee <jouanet.camille@gmail.com>
2024-09-19 10:24:22 +0200

    2 lights added

---
+++ TutoXR/Assets/LightControl.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour
+{
+    public GameObject btn_light;
+    public GameObject btn_bulb;
+    [Tooltip("button/bulb color")]
+    public Color btn_color;
+
+    [Tooltip("OFF state light intensity")]
+    public float off_intensity;
+    [Tooltip("ON state light intensity")]
+    public float on_intensity;
+
+    //Internals
+    private Light l;
+    private bool on_state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        on_state = false;
+        l = btn_light.GetComponent<Light>();
+        if (l == null)
+        {
+            Debug.Log("LightControl " + this.name + "-Error no local light defined");
+        }
+        else l.color = btn_color;
+        Renderer rend = btn_bulb.GetComponent<Renderer>();
+        if (rend == null)
+        {
+            Debug.Log("LightControl " + this.name + "-btn_bulb do not have renderer");
+        }
+        else
+        {
+            rend.material.color = btn_color;
+        }
+    }
+
+    public void Switch_On()
+    {
+        if (l != null) l.intensity = off_intensity;
+        this.on_state = true;
+    }
+
+    public void Switch_Off()
+    {
+        if (l != null) l.intensity = off_intensity;
+        this.on_state = false;
+    }
+
+    public void Switch_State()
+    {
+        if (this.on_state) this.Switch_Off();
+        else this.Switch_On();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
